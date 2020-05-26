9f880c912687d0c8e3d24b620b303841939d5838
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-05-26 00:39:29 -0400

    Add test cases for camelcase

--- src/camelcase_test.cpp
+++ src/camelcase_test.cpp
@@ -5,12 +5,34 @@
 using namespace std;
 
 TEST_CASE( "Test decoding of camel case strings", "[camelcase]" ) {
-    string s;
-
-    decodeCamelCase("CAMELCASE", &s);
-    REQUIRE(s == "camelcase");
-
-    decodeCamelCase("camelsRUs", &s);
-    REQUIRE(s == "camels_r_us");
+    vector<pair<string, string>> tests = {
+        {"", ""},
+        {"c", "c"},
+        {"C", "c"},
+        {"cc", "cc"},
+        {"Cc", "cc"},
+        {"CC", "cc"},
+        {"cC", "c_c"},
+        {"ccc", "ccc"},
+        {"Ccc", "ccc"},
+        {"CCc", "c_cc"},
+        {"CCC", "ccc"},
+        {"Ccase", "ccase"},
+        {"CCase", "c_case"},
+        {"cCase", "c_case"},
+        {"CamelC", "camel_c"},
+        {"CamelCase", "camel_case"},
+        {"camelCase", "camel_case"},
+        {"Camelcase", "camelcase"},
+        {"CAMELCase", "camel_case"},
+        {"CamelCASE", "camel_case"},
+        {"CAMELCASE", "camelcase"},
+        {"camelsRUs", "camels_r_us"}
+    };
+    for (auto& t : tests) {
+        string s;
+        decodeCamelCase(t.first.c_str(), &s);
+        REQUIRE(s == t.second);
+    }
 }
 
