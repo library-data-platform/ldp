aa95fad8460c37e237b3eb31b8f638fb6b67b290
Nassib Nassar <nrn@etymon.com>
2024-01-19 10:37:06 -0500

    Fix missing function prototype

--- src/dbup1.h
+++ src/dbup1.h
@@ -37,6 +37,7 @@ void database_upgrade_31(database_upgrade_options* opt);
 void database_upgrade_32(database_upgrade_options* opt);
 void database_upgrade_33(database_upgrade_options* opt);
 void database_upgrade_34(database_upgrade_options* opt);
+void database_upgrade_35(database_upgrade_options* opt);
 
 void ulog_sql(const string& sql, database_upgrade_options* opt);
 void ulog_commit(database_upgrade_options* opt);
